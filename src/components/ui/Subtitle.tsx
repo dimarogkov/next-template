fac3960ab6d6359864a156575aa595d8dd6f8c51
dima.rogkov <dimarogkov41475@ukr.net>
2024-06-25 15:37:42 +0300

    update ui components

--- src/components/ui/Subtitle.tsx
+++ src/components/ui/Subtitle.tsx
@@ -1,14 +1,16 @@
-type Props = {
-    children: React.ReactNode;
+import { HTMLAttributes, forwardRef } from 'react';
+
+interface Props extends HTMLAttributes<HTMLHeadingElement> {
     className?: string;
-};
+}
 
-const Subtitle: React.FC<Props> = ({ children, className = '' }) => {
-    return (
-        <h3 className={`w-full text-[20px] md:text-[24px] lg:text-[28px] xl:text-[32px] font-bold ${className}`}>
-            {children}
-        </h3>
-    );
-};
+const Subtitle: React.FC<Props> = forwardRef<HTMLHeadingElement, Props>(({ className = '', ...props }, ref) => (
+    <h3
+        ref={ref}
+        {...props}
+        className={`w-full text-[20px] md:text-[24px] lg:text-[28px] xl:text-[32px] font-bold ${className}`}
+    />
+));
 
+Subtitle.displayName = 'Subtitle';
 export default Subtitle;
