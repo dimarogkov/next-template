a2d15fabbf10edc6b6c96e5cc8a957a442c8e957
dima.rogkov <dimarogkov41475@ukr.net>
2024-11-30 15:03:31 +0200

    update all form elements validation

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,24 +1,16 @@
 import { FC, InputHTMLAttributes, RefAttributes, forwardRef } from 'react';
-import { UseFormRegister } from 'react-hook-form';
-import { EnumFormNames } from '@/src/types/enums';
-import { IFormValues } from '@/src/types/interfaces/FormValues';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement>, RefAttributes<HTMLInputElement> {
     className?: string;
-    registerName?: EnumFormNames;
-    register?: UseFormRegister<IFormValues>;
 }
 
-const Input: FC<Props> = forwardRef<HTMLInputElement, Props>(
-    ({ className = '', registerName = EnumFormNames.username, register = () => {}, ...props }, ref) => (
-        <input
-            ref={ref}
-            {...props}
-            className={`w-full h-10 px-4 rounded border border-gray bg-transparent outline-none transition-all duration-300 focus:border-black ${className}`}
-            {...register(registerName)}
-        />
-    )
-);
+const Input: FC<Props> = forwardRef<HTMLInputElement, Props>(({ className = '', ...props }, ref) => (
+    <input
+        ref={ref}
+        {...props}
+        className={`w-full h-10 px-4 rounded border border-gray bg-transparent outline-none transition-all duration-300 focus:border-black ${className}`}
+    />
+));
 
 Input.displayName = 'Input';
 export default Input;
