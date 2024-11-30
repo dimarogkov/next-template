2affeb178d6fb193b67cb2fbbe22411e85aa0837
dima.rogkov <dimarogkov41475@ukr.net>
2024-11-30 14:49:56 +0200

    remove theme

--- src/components/root/Root.tsx
+++ src/components/root/Root.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactNode } from 'react';
-import { ThemeProvider } from 'next-themes';
 import { Header } from '../elements';
 
 type Props = {
@@ -10,10 +9,8 @@ const Root: FC<Props> = ({ children }) => {
     return (
         <html lang='en' suppressHydrationWarning>
             <body>
-                <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-                    <Header />
-                    <main className='relative w-full max-w-screen-2xl p-5 m-auto'>{children}</main>
-                </ThemeProvider>
+                <Header />
+                <main className='relative w-full max-w-screen-2xl p-5 m-auto'>{children}</main>
             </body>
         </html>
     );
