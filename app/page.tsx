890b7fb5c126594342bd618fc468332c49d8a587
dima.rogkov <dimarogkov41475@ukr.net>
2025-06-12 17:33:49 +0300

    some fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
             <div className='container'>
                 <div className='w-full'>
                     <div className='flex flex-col gap-4 w-full mb-8 last:mb-0'>
-                        <Title titleType={EnumTitle.h2}>React Template</Title>
+                        <Title titleType={EnumTitle.h2}>Next Template</Title>
 
                         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 w-full'>
                             {DEV_ICONS.map((icon, text) => (
@@ -19,8 +19,8 @@ const HomePage = () => {
                         </div>
 
                         <Text textType={EnumText.large}>
-                            This is React Template for development. Technolgies used: React.js, TypeScript, Tailwind
-                            CSS, Axios, React Query, React Hook Form, Yup, Frame Motion, Redux Toolkit, Zustand.
+                            This is Next Template for development. Technolgies used: Next.js, TypeScript, Tailwind CSS,
+                            Axios, React Query, React Hook Form, Yup, Frame Motion, Redux Toolkit, Zustand.
                         </Text>
 
                         <BtnLink href='https://github.com/dimarogkov/next-template' target='_blank'>
